96a2bd35e5b82e37fe6e6f31885790256eaca3a2
Emily_Richard <richard.emily.e@gmail.com>
2019-10-30 12:37:40 -0400

    added helmet middleware

---
+++ notes.md
@@ -0,0 +1,11 @@
+# Middleware Notes
+
+**EVERYTHING IS MIDDLEWARE!!**
+
+Well, almost everything :-)
+
+## Types `(based on how we got it or who built it)`
+
+- built-in: included with express. ex: `express.json()`
+- third party: must be installed from `npm`
+- custom: we code these!!
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('./helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
